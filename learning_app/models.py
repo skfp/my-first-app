440e124369be20b80967f1743f92d57f70b89577
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-04-08 22:05:20 +0200

    second commit

--- learning_app/models.py
+++ learning_app/models.py
@@ -1,8 +1,24 @@
 from django.db import models
+from django.utils import timezone
+
+class Card(db.Model):
+    #__tablename__ = 'cards'
+
+    card_id = models.Integer()
+    pile_id = models.Integer()
+    first_lng = models.CharField(max_length=200)
+    second_lng = models.CharField(max_length=200)
+    card_type = models.CharField(max_length=1)  
+    #N-new, S-short, M-medium, L-long, H-very long, W-wrong
 
 
-class Card(models.Model):
-    front = models.CharField(max_length=100)
-    back = models.CharField(max_length=100)
+class Answer(db.Model):
+    #__tablename__ = 'answers'
+    answer_id = models.Integer()
+    card_id = models.Integer()
+    pile_id = models.Integer()
+    datestamp = models.DateTimeField(default=timezone.now)
+    answer = models.CharField(max_length=1)  
+    #E-easy, M-medium, H-hard, W-wrong
 
 
