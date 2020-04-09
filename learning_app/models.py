52279200f3fa1384570081f0e7398e1941a2a552
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-04-09 08:21:13 +0200

    card_id2

--- learning_app/models.py
+++ learning_app/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 class Card(models.Model):
     #__tablename__ = 'cards'
 
-    card_id = models.IntegerField()
-    pile_id = models.IntegerField()
+    card_id = models.IntegerField(default=0)
+    pile_id = models.IntegerField(default=0)
     first_lng = models.CharField(max_length=200)
     second_lng = models.CharField(max_length=200)
     card_type = models.CharField(max_length=1)
@@ -14,9 +14,9 @@ class Card(models.Model):
 
 class Answer(models.Model):
     #__tablename__ = 'answers'
-    answer_id = models.IntegerField()
-    card_id_ans = models.IntegerField()
-    pile_id = models.IntegerField()
+    answer_id = models.IntegerField(default=0)
+    card_id_ans = models.IntegerField(default=0)
+    pile_id = models.IntegerField(default=0)
     datestamp = models.DateTimeField(default=timezone.now)
     answer = models.CharField(max_length=1)  
     #E-easy, M-medium, H-hard, W-wrong
