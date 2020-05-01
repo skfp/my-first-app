e55af0cac16bcd68eaa5647eb44411aa61f0cd8c
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-01 18:51:06 +0200

    url

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('learn/', views.learn, name='learn'),
 ]
 
 
--- learning_app/views.py
+++ learning_app/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'learning_app/home.html', {})
 
-
+def learn(request):
+    return render(request, 'learning_app/learn.html', {})
 
 
 
