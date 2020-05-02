21cf86be147c5f455eb86cf9b3dbe8836589a1ad
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-02 12:56:55 +0200

    commit

--- learning_app/views.py
+++ learning_app/views.py
@@ -15,8 +15,6 @@ def load(request):
     data_pl_lt=virgin_data.drop(['Unnamed: 4','Unnamed: 5','Unnamed: 6','Unnamed: 7'], axis=1)
     for i in range(data_pl_lt.shape[0]):
         NewCardRecord = Card(card_id = data_pl_lt['id'][i],pile_id = 1,first_lng=data_pl_lt['pl'][i],second_lng= data_pl_lt['lt'][i],card_type=data_pl_lt['class'][i])
-        #db.session.add(NewCardRecord)
-        #db.session.commit()
         NewCardRecord.save()
     return render(request, 'learning_app/load.html', {})
 
