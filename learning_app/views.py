8b24adc7e1def99d402ab6311f6ec940a9a8bd37
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-02 12:23:43 +0200

    vvv

--- learning_app/views.py
+++ learning_app/views.py
@@ -10,7 +10,7 @@ def learn(request):
     return render(request, 'learning_app/learn.html', {})
 
 def load(request):
-    virgin_data=pd.read_csv("input.csv",sep=";")
+    virgin_data=pd.read_csv("learning_app/static/data/input.csv",sep=";")
     data_pl_lt=virgin_data.drop(['Unnamed: 4','Unnamed: 5','Unnamed: 6','Unnamed: 7'], axis=1)
     for i in range(data_pl_lt.shape[0]):
         NewCardRecord = Card(card_id = data_pl_lt['id'][i],pile_id = 1,first_lng=data_pl_lt['pl'][i],second_lng= data_pl_lt['lt'][i],card_type=data_pl_lt['class'][i])
