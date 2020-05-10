8ae947f4533725660c8ac478aa08af2b21d2c6b7
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-10 13:04:39 +0200

    ajax

---
+++ learning_app/answer_e.py
@@ -0,0 +1,15 @@
+from learning_app.models import Card,User,Answer
+
+import sys
+
+def add_answer():
+    NewAnswerRecord = ( answer_id=1,
+    card_id_ans=1,
+    pile_id=1,
+    answer="E"
+    )
+    NewAnswerRecord.save()
+
+
+if __name__ == "__main__":
+    add_answer()
