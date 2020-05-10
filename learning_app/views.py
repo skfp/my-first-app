10065af98c2ad4d3341752472ed7fae5cdb4f895
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-10 19:44:52 +0200

    c

--- learning_app/views.py
+++ learning_app/views.py
@@ -24,7 +24,6 @@ def learn(request, previous_id, previous_ans):
         NewAnswerRecord.save()
     random_id=randrange(600)
     one_card_object=Card.objects.get(card_id=random_id) 
-    one_user_object=User.objects.get(user_id=1) 
     if one_card_object.card_type in ["N","NEW"]:
         my_user = User.objects.get(user_id=1)
         my_user.new_left_today = my_user.new_left_today-1
@@ -41,6 +40,7 @@ def learn(request, previous_id, previous_ans):
         my_user = User.objects.get(user_id=1)
         my_user.wrong_left_today = my_user.wrong_left_today+1
         my_user.save()
+    one_user_object=User.objects.get(user_id=1) 
     one_context = {'one_user_object': one_user_object,'one_card_object': one_card_object}
     return render(request, 'learning_app/learn.html', one_context)
 
