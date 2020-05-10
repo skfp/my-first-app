15f3cf9345ade71b2d898df9234a205ac5991050
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-10 19:41:41 +0200

    test

--- learning_app/views.py
+++ learning_app/views.py
@@ -37,7 +37,8 @@ def learn(request, previous_id, previous_ans):
         my_user = User.objects.get(user_id=1)
         my_user.wrong_left_today = my_user.wrong_left_today-1
         my_user.save()
-    if previous_ans=="W":
+    #if previous_ans=="W":
+    if previous_ans:
         my_user = User.objects.get(user_id=1)
         my_user.wrong_left_today = my_user.wrong_left_today+1
         my_user.save()
