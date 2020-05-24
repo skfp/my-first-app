7bd26a445919be48c0b103e8e800550880904ca9
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-05-24 10:21:54 +0200

    file

--- learning_app/views.py
+++ learning_app/views.py
@@ -10,7 +10,7 @@ from django import forms
 from .forms import UploadFileForm
 
 # Imaginary function to handle an uploaded file.
-from somewhere import handle_uploaded_file
+#from somewhere import handle_uploaded_file
 
 # Create your views here.
 
@@ -168,12 +168,19 @@ def load_eng(request):
 #        NewCardRecord.save()
 #    return render(request, 'learning_app/load_any.html', {})
 
+def handle_uploaded_file(f):
+    virgin_data=pd.read_csv("learning_app/static/data/name.csv",sep=";")
+    data_pl_lt=virgin_data.drop(['Unnamed: 4','Unnamed: 5','Unnamed: 6','Unnamed: 7'], axis=1)
+    for i in range(data_pl_lt.shape[0]):
+        NewCardRecord = Card(card_id = data_pl_lt['id'][i],pile_id = 2,first_lng=data_pl_lt['pl'][i],second_lng= data_pl_lt['eng'][i],card_type=data_pl_lt['class'][i])
+        NewCardRecord.save()
+
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
+            return HttpResponseRedirect('/home/')
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
