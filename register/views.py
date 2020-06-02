5e4242f1b295a7c8219f601b9040073affa086f5
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-06-02 19:29:35 +0200

    indent

--- register/views.py
+++ register/views.py
@@ -6,7 +6,7 @@ from .forms import RegisterForm
 # Create your views here.
 def register(response):
     if response.method == "POST":
-	form = RegisterForm(response.POST)
+	    form = RegisterForm(response.POST)
 	if form.is_valid():
 	    form.save()
 
