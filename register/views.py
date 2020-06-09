fd160870ef7b52ef7ee78d936638e0ccf37e334e
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-06-09 20:10:10 +0200

    c

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            UserManager.create_user(form.username,form.email,form.password1)
+            #UserManager.create_user(form.username,form.email,form.password1)
         return redirect("/home")
     else:
     	form = RegisterForm()
