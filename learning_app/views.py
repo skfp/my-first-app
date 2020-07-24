fa75173da8bad23398e8cbb06e9d3988e7084f59
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-07-24 13:53:41 +0200

    changing order of lines in view learn

--- learning_app/views.py
+++ learning_app/views.py
@@ -64,43 +64,23 @@ def learn(request, user_id, pile_id, previous_id, previous_ans):
                     NewAnswerRecord.save()
                     previous_card_object=Card.objects.get(card_id=previous_id, pile_id=pile_id) 
                 
-                #my_pile = Pile.objects.get(pile_id=pile_id)
                 is_last=False
-                #random_id=randrange(8)+1
                 
-                #possible_cards: nowe + zle + zwykle_z_dzisiejsza_data      datetime.date(2005, 1, 1))
-                possible_cards_new=Card.objects.filter(pile_id=pile_id,card_type__in=["NEW","N"]) 
-                possible_cards_normal=Card.objects.filter(pile_id=pile_id,card_type__in=["S","M","L","H"],next_learn_date=date.today())
-                possible_cards_wrong=Card.objects.filter(pile_id=pile_id,card_type="W") 
-                #possible_cards=Card.objects.get(card_id=random_id, pile_id=pile_id)
-                #count_new=max(0,len(possible_cards_new))
-                count_new=min(my_pile.new_left_today,len(possible_cards_new))
-                count_normal=len(possible_cards_normal)
-                count_wrong=len(possible_cards_wrong)
-                count_possible=count_new+count_normal+count_wrong
-                random_number=randrange(count_possible)
-                if random_number + 1 <= count_new:
-                    id_list=[x.card_id for x in possible_cards_new]
-                    random_id=id_list[randrange(len(possible_cards_new))]
-                elif count_normal + count_new >= random_number + 1 > count_new:
-                    id_list=[x.card_id for x in possible_cards_normal]
-                    random_id=id_list[randrange(len(possible_cards_normal))]
-                else:
-                    id_list=[x.card_id for x in possible_cards_wrong]
-                    random_id=id_list[randrange(len(possible_cards_wrong))]
-                one_card_object=Card.objects.get(card_id=random_id, pile_id=pile_id)
-
                 if my_pile.new_left_today + my_pile.normal_left_today + my_pile.wrong_left_today == 1:
                     is_last=True
-                if one_card_object.card_type in ["N","NEW"]:
-                    my_pile.new_left_today = my_pile.new_left_today-1
-                    my_pile.save()
-                if one_card_object.card_type in ["M","H"]:
-                    my_pile.normal_left_today = my_pile.normal_left_today-1
-                    my_pile.save()
-                if one_card_object.card_type == "W":
-                    my_pile.wrong_left_today = my_pile.wrong_left_today-1
-                    my_pile.save()
+                if previous_ans!="X":
+                    if previous_card_object.card_type in ["N","NEW"]:
+                    #if one_card_object.card_type in ["N","NEW"]:
+                        my_pile.new_left_today = my_pile.new_left_today-1
+                        my_pile.save()
+                    if previous_card_object.card_type in ["M","H"]:
+                    #if one_card_object.card_type in ["M","H"]:
+                        my_pile.normal_left_today = my_pile.normal_left_today-1
+                        my_pile.save()
+                    if previous_card_object.card_type == "W":
+                    #if one_card_object.card_type == "W":
+                        my_pile.wrong_left_today = my_pile.wrong_left_today-1
+                        my_pile.save()
                 if previous_ans=="W":
                     my_pile.wrong_left_today = my_pile.wrong_left_today+1
                     my_pile.save()
@@ -123,6 +103,29 @@ def learn(request, user_id, pile_id, previous_id, previous_ans):
                     next_tz=timezone.now() + timezone.timedelta(days=2)
                     previous_card_object.next_learn_date=date(next_tz.year,next_tz.month,next_tz.day)
                     previous_card_object.save()
+
+                #possible_cards: nowe + zle + zwykle_z_dzisiejsza_data      datetime.date(2005, 1, 1))
+                possible_cards_new=Card.objects.filter(pile_id=pile_id,card_type__in=["NEW","N"]) 
+                possible_cards_normal=Card.objects.filter(pile_id=pile_id,card_type__in=["S","M","L","H"],next_learn_date=date.today())
+                possible_cards_wrong=Card.objects.filter(pile_id=pile_id,card_type="W") 
+                #possible_cards=Card.objects.get(card_id=random_id, pile_id=pile_id)
+                #count_new=max(0,len(possible_cards_new))
+                count_new=min(my_pile.new_left_today,len(possible_cards_new))
+                count_normal=len(possible_cards_normal)
+                count_wrong=len(possible_cards_wrong)
+                count_possible=count_new+count_normal+count_wrong
+                random_number=randrange(count_possible)
+                if random_number + 1 <= count_new:
+                    id_list=[x.card_id for x in possible_cards_new]
+                    random_id=id_list[randrange(len(possible_cards_new))]
+                elif count_normal + count_new >= random_number + 1 > count_new:
+                    id_list=[x.card_id for x in possible_cards_normal]
+                    random_id=id_list[randrange(len(possible_cards_normal))]
+                else:
+                    id_list=[x.card_id for x in possible_cards_wrong]
+                    random_id=id_list[randrange(len(possible_cards_wrong))]
+                one_card_object=Card.objects.get(card_id=random_id, pile_id=pile_id)
+
                 one_pile_object=Pile.objects.get(pile_id=pile_id)
                 user_id=one_pile_object.user_id
                 one_context = {'one_card_object': one_card_object, 'one_pile_object':one_pile_object, 'is_last':is_last,'user_id':user_id, 'current_user_id':current_user_id }
