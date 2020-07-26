fc45e774717c4a6f3245e7d104bfc4d83e31ff66
Sylwester Piątek <sylwester.piatek@pwr.edu.pl>
2020-07-26 10:39:48 +0200

    django.contrib.sites in settings

--- mysite01/settings.py
+++ mysite01/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "crispy_forms",
+    'django.contrib.sites',
+    'crispy_forms',
     'learning_app',
     'register.apps.RegisterConfig',
 ]
